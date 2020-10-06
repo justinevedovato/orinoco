9db17ccdc2d717758215eaa562304075863074d0
Justine Vedovato <justinevedovato@gmail.com>
2020-10-06 16:19:33 +0200

    fetch avec ID pour la fiche produit, formatage du prix

--- script.js
+++ script.js
@@ -3,23 +3,25 @@ const $itemCard = document.getElementById("item-card");
 const $addToCartBtn = document.getElementById("add-to-cart");
 
 // Récupérer les items depuis l'API et les intégrer au DOM
-fetch("http://localhost:3000/api/cameras")
+fetch("http://localhost:3000/api/teddies")
   .then((response) => response.json())
   .then((itemsArray) => {
     $itemCard.innerHTML = itemsArray
       .map(
         (
           item
-        ) => `<a class="card col-12 col-md-6 col-lg-4" href="/pages/product.html?id=${item._id}">
+        ) => `<a class="card col-12 col-md-6 col-lg-4" href="/pages/product.html?id=${
+          item._id
+        }">
     <figure>
-        <img
-      class="card-img-top"
-      src="${item.imageUrl}"
-    />
+          <img
+        class="card-img-top"
+        src="${item.imageUrl}"
+      />
     </figure>
     <div class="card-body">
-      <h3 class="card-title">${item.name}</h3>
-      <p class="price">${item.price} €</p>
+    <h3 class="card-title">${item.name}</h3>
+    <p class="price">${format(item.price, thd, dec)}</p>
       <!-- <button class="cart btn btn-info" id="add-to-cart">Ajouter au panier</button> -->
     </div>
   </a>`
--- scripts/product.js
+++ scripts/product.js
@@ -10,4 +10,21 @@ if (!id) {
 }
 
 // Récupérer l'item de l'API avec l'ID obtenue
-fetch("http://localhost:3000/api/cameras").then((response) => response.json());
+fetch("http://localhost:3000/api/teddies/" + id)
+  .then((response) => response.json())
+  .then((item) => {
+    $itemPage.innerHTML = `<div class="row">
+          <img class="col-12 col-md-5" src="${item.imageUrl}" />
+          <div class="col-12 col-md-7">
+            <h1 class="mt-3 mt-md-1">${item.name}</h1>
+            <p class="price h5 m-5">${format(item.price, thd, dec)}</p>
+            <button class="cart btn btn-info">Ajouter au panier</button>
+          </div>
+        </div>
+        <div class="row">
+          <h2 class="col-12 mt-4 mb-2 text-left h3">Description</h2>
+          <p class="text-justify p-3 mb-5">
+            ${item.description}
+          </p>
+        </div>`;
+  });
