0ff166d25cb142fa4aec615ac600e67e101c7218
Justine Vedovato <justinevedovato@gmail.com>
2020-10-13 18:33:15 +0200

    Amélioration du panier, merge des items identiques et quantité d'articles, suppression d'un article

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,5 +1,6 @@
 const $cartList = document.getElementById("cart-list");
 const $total = document.getElementById("total");
+const $quantity = document.getElementById("quantity");
 
 // Formater le prix
 function format(p) {
@@ -8,35 +9,81 @@ function format(p) {
 }
 
 // Récupérer les articles depuis le Local Storage pour charger le panier
-let retrievedItem = [];
-retrievedItem = JSON.parse(localStorage.getItem("cart"));
+let retrievedItems = JSON.parse(localStorage.getItem("cart"));
 
 // Calcule le nombre d'articles dans le panier:
-let totalItems = retrievedItem.length - 1;
+let totalItems = Object.values(retrievedItems).reduce(
+  (a, b) => a + b.quantity,
+  0
+);
 
-// Calcule le total du montant des articles du panier
-let allPrices = [];
-for (let i = 0; i < retrievedItem.length; i++) {
-  allPrices.push(retrievedItem[i].price);
-  totalPrices = allPrices.reduce((a, b) => a + b, 0);
+// Calcule le total du montant des articles du panier:
+let totalPrice = 0;
+let productHTML = `
+  <div class="row mx-1 mx-xl-5">
+    <div class=" col-2 col-md-1"></div>
+    <div class="col-6 col-md-5">
+      <p class="text-left pl-3 bg-light rounded"> Nom du produit </p>
+    </div>
+    <div class="col-2 col-md-3 d-none d-md-block">
+      <p class=" text-right pr-3 bg-light rounded"> Prix à l'unité </p>
+    </div>
+    <div class="col-3 col-md-2">
+      <p class="text-right pr-3 bg-light rounded"> Prix total </p>
+    </div>
+    <span class="col-1"></span>
+  </div>`;
+
+// Merge les articles identiques et itère la quantité:
+for (let id in retrievedItems) {
+  let product = retrievedItems[id];
+  totalPrice += product.price * product.quantity;
+  productHTML += `
+    <div class="row mx-1 mx-xl-5">
+    <figure class="embed-responsive embed-responsive-1by1 col-2 col-md-1">
+      <a href="/pages/product.html?id=${product.id}">
+        <img
+        class="img-fluid img-thumbnail embed-responsive-item "
+        src="${product.image}"
+        />
+      </a>
+    </figure>
+    <div class="col-6 col-md-5">
+      <p class="h5 card-title col-12 text-left my-2">${product.name}</p>
+      <p class="card-title col-12 text-left">Quantité: ${product.quantity}</p>
+    </div>
+    <div class="col-2 col-md-3 d-none d-md-block">
+      <p class="price text-right mt-3 pr-3">${format(product.price)} </p>
+    </div>
+    <div class=" col-3 col-md-2">
+      <p class="price pr-3 text-right mt-3">${format(
+        product.price * product.quantity
+      )} </p>
+        </div>
+      <span class="col-1 pt-1">
+      <i class="fas fa-trash-alt remove-btn mt-3" data-id="${product.id}"></i>
+      </span>
+    </div>`;
 }
 
-$cartList.innerHTML = retrievedItem
-  .map(
-    (item) => `<div class="row m-1 ml-lg-5 mr-lg-5">
-<figure class="img-thumbnail embed-responsive embed-responsive-1by1 col-1">
-    <img
-    class="embed-responsive-item"
-    src="${item.image}"
-  />
-</figure>
-<div class="col-9">
-<p class="h5 card-title col-12 text-left">${item.name}</p>
-<p class="card-title col-12 text-left">Quantité: ${item.amount}</p>
-</div>
-<p class="price col-2 text-right">${format(item.price)}</p>
-</div>`
-  )
-  .join("");
+$cartList.innerHTML = productHTML;
 
-$total.innerHTML = "Total: " + format(totalPrices);
+$total.innerHTML = `
+    <p class="h5"> Total: ${format(totalPrice)}</p>
+    <p class="h6 m-0">(${totalItems} articles)
+  `;
+
+// Supprime un article du panier (array "cart") et update localstorage
+const $removeBtns = document.querySelectorAll(".remove-btn");
+
+function removeItem(e) {
+  let product = retrievedItems[e.target.dataset.id]; // Récuperer l'ID du produit ciblé
+  product.quantity--;
+  if (!product.quantity) {
+    delete retrievedItems[e.target.dataset.id];
+  }
+  localStorage.setItem("cart", JSON.stringify(retrievedItems));
+  location.reload(); // refresh la page
+}
+
+$removeBtns.forEach(($button) => $button.addEventListener("click", removeItem));
--- scripts/product.js
+++ scripts/product.js
@@ -4,25 +4,13 @@ const $itemPage = document.getElementById("item-page");
 const params = new URLSearchParams(document.location.search);
 let id = params.get("id");
 
-// Si aucun ID dans l'URL, envoi vers une éventuelle page d'erreur
-if (!id) {
-  // document.location.href = "404.html";
-  $itemPage.innerHTML = `<div class="text-center m-2">
-  <p class="display-4">404</p>
-  <p class="h3">La page que vous recherchez n'existe pas!</p>
-  <a href="javascript:history.go(-1)"
-    ><button class="btn btn-info m-3">Retour</button></a
-  >
-</div>`;
-}
-
 // Récupérer l'item de l'API avec l'ID obtenue
 fetch("http://localhost:3000/api/teddies/" + id)
   .then((response) => response.json())
   .then((item) => {
     let colors = "";
     item.colors.forEach((color) => {
-      // colors += `<li style="background:${color}" alt="${color}"></li>`;
+      // récupère toutes les options de couleur de chaque article
       colors += `<li class="dropdown-item">${color}</li>`;
     });
 
@@ -50,13 +38,24 @@ fetch("http://localhost:3000/api/teddies/" + id)
           </p>
         </div>`;
 
+    // Si aucun ID dans l'URL, envoi vers une éventuelle page d'erreur
+    if (!item._id) {
+      // document.location.href = "404.html";
+      $itemPage.innerHTML = `<div class="text-center m-2">
+  <p class="display-4">404</p>
+  <p class="h3">La page que vous recherchez n'existe pas!</p>
+  <a href="../index.html"
+    ><button class="btn btn-info m-3">Retour à l'accueil</button></a
+  >
+</div>`;
+    }
     // CHOIX DE L'OPTION
     // Ajouter la classe "selected" quand on selectionne une couleur, et déselectionne quand on choisit une autre couleur, ou qu'on re-clique sur la couleur en question pour le déselectionner
     const $colorElements = document.querySelectorAll(".colors-list li");
     const $optionBtn = document.getElementById("option-btn");
 
-    $colorElements.forEach(($colorEl) => {
-      $colorEl.addEventListener("click", (e) => {
+    $colorElements.forEach(($colorElement) => {
+      $colorElement.addEventListener("click", (e) => {
         const isSelected = e.target.classList.contains("active"); // vérifie les elements qui ont été selectionnés
 
         $colorElements.forEach((el) => el.classList.remove("active")); // desélectionne tout ce qui a été auparavant selectionné pour ne selectionner qu'un seul item à chaque fois
@@ -64,7 +63,7 @@ fetch("http://localhost:3000/api/teddies/" + id)
         if (!isSelected) {
           e.target.classList.add("active"); // Ajoute la classe 'selected' sur l'élément choisi, s'il n'a pas été selected avant
         }
-        $optionBtn.innerText = $colorEl.innerText + " "; // Affiche la couleur choisie dans le bouton
+        $optionBtn.innerText = $colorElement.innerText + " ";
       });
     });
 
@@ -72,7 +71,7 @@ fetch("http://localhost:3000/api/teddies/" + id)
     const $msg = document.getElementById("message");
 
     // LOCAL STORAGE
-    let cart = []; // Démarre avec un array vide
+    let cart = {}; // Démarre avec un object vide
     let retrievedItem = localStorage.getItem("cart");
 
     // Vérifie si le Local Storage comporte des items, si oui, les ajoute à l'array "cart":
@@ -87,9 +86,17 @@ fetch("http://localhost:3000/api/teddies/" + id)
         name: item.name,
         price: item.price,
         image: item.imageUrl,
-        amount: 1,
+        quantity: 1,
       };
-      cart.push(product);
+
+      // Vérifier si l'article est déjà dans le panier:
+      if (cart[item._id]) {
+        cart[item._id].quantity++; // Itérer la quantité si l'article est déjà dans le panier
+      } else {
+        cart[item._id] = product; // Ajouter l'article au local storage
+      }
+
+      // Sauvegarder le panier au localstorage
       localStorage.setItem("cart", JSON.stringify(cart));
       $msg.innerHTML = "L'article a bien été ajouté au panier";
     }
